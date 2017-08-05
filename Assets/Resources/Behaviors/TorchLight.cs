c002b14581d601e7db7f6232c951c4f2d641598b
Andrew McPherson <ehgoodenough@gmail.com>
2017-08-05 16:13:45 -0700

    Refactored prefab as it's own prefab.

---
+++ Assets/Resources/Behaviors/TorchLight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchLight : MonoBehaviour {
+	void Update () {
+		if(Hero.instance == null) {
+			return;
+		}
+
+		Vector3 position = Hero.instance.transform.position;
+		position.z = -1;
+		this.transform.position = position;
+	}
+}
