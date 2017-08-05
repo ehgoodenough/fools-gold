dc17a2ae29d336409af1ca30ca63a03caf06db41
Andrew McPherson <ehgoodenough@gmail.com>
2017-08-04 20:27:44 -0700

    Added the hero moving across the screen.

---
+++ Assets/Behaviors/Hero.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+	private Vector3 targetPosition = new Vector3();
+	private float speed = 10;
+
+	void Update() {
+
+		if(Input.GetKeyDown("up")) {
+			this.targetPosition += Vector3.up;
+		}
+		if(Input.GetKeyDown("down")) {
+			this.targetPosition += Vector3.down;
+		}
+		if(Input.GetKeyDown("left")) {
+			this.targetPosition += Vector3.left;
+		}
+		if(Input.GetKeyDown("right")) {
+			this.targetPosition += Vector3.right;
+		}
+
+		float step = Vector3.Distance(this.transform.position, this.targetPosition) * this.speed * Time.deltaTime;
+        this.transform.position = Vector3.MoveTowards(this.transform.position, this.targetPosition, step);
+	}
+}
