3d195d88fe8e97d6a40adcdb8362e643630f8c56
Andrew McPherson <ehgoodenough@gmail.com>
2017-08-04 21:26:51 -0700

    Added collision with the tiles.

--- Assets/Resources/Behaviors/Map.cs
+++ Assets/Resources/Behaviors/Map.cs
@@ -8,23 +8,35 @@ public class Map : MonoBehaviour {
 	private Object tileObject;
 	public static Map instance;
 
+	private Hashtable tiles = new Hashtable();
+
 	void Awake() {
 		instance = this;
-		this.tileObject = Resources.Load("Prefabs/Tile") as Object;
+		tileObject = Resources.Load("Prefabs/Tile") as Object;
 	}
 
 	void Start() {
-		this.addTile(new Vector2(-1, -1));
+		this.addTile(new Vector3(-1, -1, 10));
+		this.addTile(new Vector3(-1, 0, 10));
+		this.addTile(new Vector3(-1, 1, 10));
+		this.addTile(new Vector3(-1, 2, 10));
+		this.addTile(new Vector3(0, -1, 10));
+		this.addTile(new Vector3(1, -1, 10));
+		this.addTile(new Vector3(2, -1, 10));
 	}
 
-	void addTile(Vector2 position) {
+	void addTile(Vector3 position) {
+
+		// Instantiate in the scene.
 		Transform parent = this.transform;
 		Quaternion rotation = Quaternion.identity;
-		Object.Instantiate(this.tileObject, position, rotation, parent);
-		// TODO: Add more logic here for pulling an image from a tileset.
+		Object tile = Object.Instantiate(this.tileObject, position, rotation, parent);
+
+		// Keep track of it in our hashtable.
+		this.tiles.Add(position.x + "-" + position.y, tile);
 	}
 
-	public bool canMoveTo() {
-		return true;
+	public bool canMoveTo(Vector2 position) {
+		return this.tiles[position.x + "-" + position.y] == null;
 	}
 }
