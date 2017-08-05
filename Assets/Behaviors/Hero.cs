d93541ac7518c9152b97c684f2affdb422b22464
Andrew McPherson <ehgoodenough@gmail.com>
2017-08-04 20:34:35 -0700

    Added a collision check to the hero.

--- Assets/Behaviors/Hero.cs
+++ Assets/Behaviors/Hero.cs
@@ -8,20 +8,43 @@ public class Hero : MonoBehaviour {
 
 	void Update() {
 
+		// Poll the keyboard.
 		if(Input.GetKeyDown("up")) {
-			this.targetPosition += Vector3.up;
+			if(this.canMoveTo(this.targetPosition + Vector3.up)) {
+				this.targetPosition += Vector3.up;
+			}
 		}
 		if(Input.GetKeyDown("down")) {
-			this.targetPosition += Vector3.down;
+			if(this.canMoveTo(this.targetPosition + Vector3.down)) {
+				this.targetPosition += Vector3.down;
+			}
 		}
 		if(Input.GetKeyDown("left")) {
-			this.targetPosition += Vector3.left;
+			if(this.canMoveTo(this.targetPosition + Vector3.left)) {
+				this.targetPosition += Vector3.left;
+			}
 		}
 		if(Input.GetKeyDown("right")) {
-			this.targetPosition += Vector3.right;
+			if(this.canMoveTo(this.targetPosition + Vector3.right)) {
+				this.targetPosition += Vector3.right;
+			}
 		}
 
+		// Move the position to the target position.
 		float step = Vector3.Distance(this.transform.position, this.targetPosition) * this.speed * Time.deltaTime;
         this.transform.position = Vector3.MoveTowards(this.transform.position, this.targetPosition, step);
+
+		// If we've reached out target position, then stop moving.
+		if(Vector3.Distance(this.transform.position, this.targetPosition) < 0.01) {
+			this.transform.position = this.targetPosition;
+		}
+	}
+
+	private bool canMoveTo(Vector3 position) {
+		if(position.x < -3) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
