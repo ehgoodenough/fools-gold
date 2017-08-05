78741ec184ea60e67507eeefba2d13bfb547ec9c
Andrew McPherson <ehgoodenough@gmail.com>
2017-08-04 21:06:56 -0700

    Instantiated tiles in the map.

--- Assets/Resources/Behaviors/Map.cs
+++ Assets/Resources/Behaviors/Map.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Map : MonoBehaviour {
 
+
+	private Object tileObject;
 	public static Map instance;
 
 	void Awake() {
 		instance = this;
+		this.tileObject = Resources.Load("Prefabs/Tile") as Object;
 	}
 
-	private Texture2D texture;
-
 	void Start() {
-		texture = Resources.Load("Images/square") as Texture2D;
-		Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f);
+		this.addTile(new Vector2(-1, -1));
+	}
+
+	void addTile(Vector2 position) {
+		Transform parent = this.transform;
+		Quaternion rotation = Quaternion.identity;
+		Object.Instantiate(this.tileObject, position, rotation, parent);
+		// TODO: Add more logic here for pulling an image from a tileset.
 	}
 
 	public bool canMoveTo() {
