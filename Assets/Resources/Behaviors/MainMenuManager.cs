5edfbbf0a88b995705cac73d1d6833f292b3f8dd
aphermus <aphermus@gmail.com>
2017-08-05 18:35:56 -0700

    Create official dungeon scene and link it to title scene

--- Assets/Resources/Behaviors/MainMenuManager.cs
+++ Assets/Resources/Behaviors/MainMenuManager.cs
@@ -7,7 +7,6 @@ public class MainMenuManager : MonoBehaviour {
 
     public void OnStartGame()
     {
-        // TODO load level 1
-        SceneManager.LoadScene("Other Andrew's Playground");
+        SceneManager.LoadScene("Dungeon");
     }
 }
