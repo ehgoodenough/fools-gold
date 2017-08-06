506ece06326579574d3d72d73f70ea4a4f172a57
Matan Halberstadt <tzmtnh@gmail.com>
2017-08-06 14:24:06 -0700

    minor fix

--- Assets/Resources/Behaviors/PanningCamera.cs
+++ Assets/Resources/Behaviors/PanningCamera.cs
@@ -7,6 +7,11 @@ public class PanningCamera : MonoBehaviour {
 	Vector3 velocity = Vector3.zero;
 	private float smoothTime = 0.33f;
 
+	void Awake() {
+		Camera cam = GetComponent<Camera>();
+		cam.farClipPlane = 2000;
+	}
+
 	void Update() {
 		this.transform.position = Vector3.SmoothDamp(this.transform.position, Hero.instance.targetPos, ref this.velocity, smoothTime);
 		this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, -200);
