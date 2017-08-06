38f739e76991fd9a2a779e1a18dd9dfae79ccbfb
aphermus <aphermus@gmail.com>
2017-08-05 17:05:55 -0700

    Main menu scene

---
+++ Assets/Resources/Behaviors/MainMenuManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public void OnStartGame()
+    {
+        // TODO load level 1
+        SceneManager.LoadScene("Other Andrew's Playground");
+    }
+}
