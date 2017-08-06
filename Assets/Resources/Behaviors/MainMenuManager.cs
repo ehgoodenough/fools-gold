820493e99e1cbcd486d1b7857feb2d37e833be29
aphermus <aphermus@gmail.com>
2017-08-05 22:37:19 -0700

    Start on credits and attribution page
    Sections placed basically randomly, names listed alphabetically within
    section.

--- Assets/Resources/Behaviors/MainMenuManager.cs
+++ Assets/Resources/Behaviors/MainMenuManager.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject menu;
+
+    [SerializeField]
+    private GameObject credits;
+
     public void OnStartGame()
     {
         SceneManager.LoadScene("Dungeon");
+    }
+
+    public void OnCredits()
+    {
+        menu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void OnBack()
+    {
+        menu.SetActive(true);
+        credits.SetActive(false);
     }
 }
